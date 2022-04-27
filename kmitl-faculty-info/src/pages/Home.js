f680bd71bf35447b74a6eb3d618c04c042bfbf5c
rapeepatpe <54279530+rapeepatpe@users.noreply.github.com>
2022-04-28 03:28:42 +0700

    Landingpage 40%

--- kmitl-faculty-info/src/pages/Home.js
+++ kmitl-faculty-info/src/pages/Home.js
@@ -1,3 +1,173 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const drawerWidth = 400;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    flexShrink: 0,
+    width: drawerWidth
+  },
+  drawerPaper: {
+    width: drawerWidth
+    ,marginTop : 60
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  },
+  toolbar: {
+    ...theme.mixins.toolbar,
+    [theme.breakpoints.down("sm")]: {
+      display: "none"
+    }
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3)
+    ,marginTop : 100
+  }
+}));
+
 export default function Home() {
-  return (<div>Home</div>);
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
+
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setOpen(!open);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar style={{height:120}}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawer}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          
+          <div style={{marginLeft:125}}>
+          <Typography variant="h6" noWrap style={{fontSize:40}}>
+            คณะ
+          </Typography>
+          </div>
+
+          <div style={{marginLeft:"47vw"}}>
+            
+          <Typography variant="h6" noWrap style={{fontSize:40}}>
+            สาขาวิชา
+          </Typography>
+            
+          </div>
+          
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant={isMdUp ? "permanent" : "temporary"}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        anchor="left"
+        open={open}
+        onClose={toggleDrawer}
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {["วิศวกรรมศาสตร์", "สถาปัตยกรรม ศิลปะและการออกแบบ", "ครุศาสตร์อุตสาหกรรมและเทคโนโลยี", "เทคโนโลยีการเกษตร", "วิทยาศาสตร์","อุตสาหกรรมอาหาร","เทคโนโลยีสารสนเทศ","บริหารธุรกิจ","ศิลปศาสตร์","แพทยศาสตร์","วิทยาศาสตร์ประยุกต์และการบริหารธุรกิจ" ].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText disableTypography 
+              primary={<Typography style={{fontSize:25 }}>{text}</Typography>} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        
+        <Typography component="h2" variant="h6" gutterBottom>
+          On small and extra-small screens the sidebar/drawer is temporary and
+          can be opened via the menu icon in the toolbar.
+        </Typography>
+        <Typography component="h2" variant="h6" gutterBottom>
+          On medium, large, and extra-large screens the sidebar/drawer is
+          permanent and there is no menu icon in the toolbar.
+        </Typography>
+        <hr />
+        <Typography paragraph >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
+  );
 }
+
